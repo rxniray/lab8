6c853e2b7c56e49556fad31c6c1c4f9f2aa8bd24
rxniray <93185886+rxniray@users.noreply.github.com>
2022-11-14 19:25:08 +0200

    lab8

--- showAutoCompanysPlantsDealer.php
+++ showAutoCompanysPlantsDealer.php
@@ -21,13 +21,13 @@
     }
 
     /* Надсилаємо запит серверу */
-    if($result = $mysqli->query('SELECT cc.idcar_company, cc.name_car_company, p.id_plant, p.name_plant, d.id_dealer, d.name_dealer FROM car_companys AS cc LEFT JOIN plants AS p ON cc.id_plant = p.id_plant INNER JOIN dealers AS d ON cc.id_dealer = d.id_dealer ORDER BY cc.name_car_company;')) {  
+    if($result = $mysqli->query('SELECT  cc.name_car_company,  p.name_plant,  d.name_dealer FROM car_companys AS cc LEFT JOIN plants AS p ON cc.id_plant = p.id_plant INNER JOIN dealers AS d ON cc.id_dealer = d.id_dealer ORDER BY cc.name_car_company;')) {  
 
         printf("<h3>Звіт - Список компаній, заводів і дилерів: </h3><br>");   // <br> в html - розрив рядка
-        printf("<table><tr><th>ID-компанії</th><th>Назва компанії</th><th>ID-заводу</th><th>Назва заводу</th><th>ID-дилера</th><th>Назва дилера</th></th>");   // <br> в html - розрив рядка
+        printf("<table><tr><th>Назва компанії</th><th>Назва заводу</th><th>Назва дилера</th></th>");   // <br> в html - розрив рядка
         /* Вибірка результатів запиту  */
         while( $row = $result->fetch_assoc() ){ // fetch_assoc() повертає рядок із запиту у вигляді асоціативного масиву, наприклад $row = ['id'=>'1', 'pib'=>'Олександр', 'grupa'=>'ІПЗ-31']
-            printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $row['idcar_company'], $row['name_car_company'], $row['id_plant'], $row['name_plant'], $row['id_dealer'], $row['name_dealer']); //виводимо результат на сторінку
+            printf("<tr><td>%s</td><td>%s</td><td>%s</td></tr>", $row['name_car_company'],  $row['name_plant'],  $row['name_dealer']); //виводимо результат на сторінку
         };
         printf("</table>");
         /*Звільняємо пам'ять*/
--- showDealersClientsCars.php
+++ showDealersClientsCars.php
@@ -21,13 +21,13 @@
     }
 
     /* Надсилаємо запит серверу */
-    if($result = $mysqli->query('SELECT d.id_dealer ,d.name_dealer, c.id_client, c.name, c.lastname, cars.id_car, cars.mark, cars.model, cars.color FROM dealers AS d LEFT JOIN clients AS c ON d.id_client = c.id_client INNER JOIN cars ON d.id_car = cars.id_car ORDER BY d.name_dealer;')) {   // $mysqli - наш об'єкт, через який здійснюємо підключення, query - метод, який дозволяє виконати довільний запит
+    if($result = $mysqli->query('SELECT  d.name_dealer,  c.name, c.lastname,  cars.mark, cars.model, cars.color FROM dealers AS d LEFT JOIN clients AS c ON d.id_client = c.id_client INNER JOIN cars ON d.id_car = cars.id_car ORDER BY d.name_dealer;')) {   // $mysqli - наш об'єкт, через який здійснюємо підключення, query - метод, який дозволяє виконати довільний запит
 
         printf("<h3>Звіт - Список дилерів, клієнтів і машин: </h3><br>");   // <br> в html - розрив рядка
-        printf("<table><tr><th>ID-Дилера</th><th>Назва дилера</th><th>ID-Клієнта</th><th>Ім'я</th><th>Призвище</th><th>ID-Машини</th><th>Марка</th><th>Модель</th><th>Колір</th></tr>");   // <br> в html - розрив рядка
+        printf("<table><tr><th>Назва дилера</th><th>Ім'я</th><th>Призвище</th><th>Марка</th><th>Модель</th><th>Колір</th></tr>");   // <br> в html - розрив рядка
         /* Вибірка результатів запиту  */
         while( $row = $result->fetch_assoc() ){ // fetch_assoc() повертає рядок із запиту у вигляді асоціативного масиву, наприклад $row = ['id'=>'1', 'pib'=>'Олександр', 'grupa'=>'ІПЗ-31']
-            printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>", $row['id_dealer'], $row['name_dealer'], $row['id_client'], $row['name'], $row['lastname'], $row['id_car'], $row['mark'], $row['model'], $row['color'] ); //виводимо результат на сторінку
+            printf("<tr><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td><td>%s</td></tr>",  $row['name_dealer'],  $row['name'], $row['lastname'], $row['mark'], $row['model'], $row['color'] ); //виводимо результат на сторінку
         };
         printf("</table>");
         /*Звільняємо пам'ять*/
